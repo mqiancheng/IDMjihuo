25c3630dbc78679c48bf834a799955b4404e1ec8
mqiancheng <281183595@qq.com>
2024-11-21 15:31:39 +0800

    Update IAS.ps1

--- IAS.ps1
+++ IAS.ps1
@@ -5,6 +5,7 @@ $ErrorActionPreference = "Stop"
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
 $DownloadURL = 'https://raw.githubusercontent.com/mqiancheng/IDMjihuo/refs/heads/main/IAS.cmd'
+$DownloadURL2 = 'https://bitbucket.org/WindowsAddict/idm-activation-script/raw/main/IAS.cmd'
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
